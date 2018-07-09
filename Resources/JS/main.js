9a7c35ca6460e1433b6a7f9498cac36c59e01ddb
PeteHass94 <phassard@hotmail.co.uk>
2018-07-09 21:33:11 +0100

    matchday update

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -1,13 +1,16 @@
 $(document).on("scroll", function(){
+  var element = document.getElementById("bumper");
   if
-    ($(document).scrollTop() > 130){
+    ($(document).scrollTop() > 150){
     $("nav").addClass("shrink");
     $("header").addClass("shrink");
+    $(element).addClass("shrink");
   }
   if
     ($(document).scrollTop() < 130){
     $("nav").removeClass("shrink");
     $("header").removeClass("shrink");
+    $(element).removeClass("shrink");
   }
 });
 $(document).ready(function(){
