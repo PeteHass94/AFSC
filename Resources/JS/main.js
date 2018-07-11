a747aa6d9c5f2a82a368b86fb2729413a7ca030b
PeteHass94 <phassard@hotmail.co.uk>
2018-07-11 20:37:43 +0100

    update july 1

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -1,9 +1,12 @@
 $(document).on("scroll", function(){
   var element = document.getElementById("bumper");
   if
+    ($(document).scrollTop() > 100){
+    $("header").addClass("shrink");
+  }
+  if
     ($(document).scrollTop() > 150){
     $("nav").addClass("shrink");
-    $("header").addClass("shrink");
     $(element).addClass("shrink");
   }
   if
