231f127ea4cc3c00e0a6f62b1a662e33cf11885b
PeteHass94 <phassard@hotmail.co.uk>
2018-08-06 20:23:49 +0100

    update august event

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -29,6 +29,18 @@ $(function () {
         $('#joinModal').modal('show');
     });
 });
+$(function () {
+    $('#commentModal').modal({
+        keyboard: true,
+        backdrop: "fixed",
+        show: false,
+    }).on('show', function () {
+    });
+    $(".commentClub").on('click', function () {
+        debugger;
+        $('#commentModal').modal('show');
+    });
+});
 
 $(document).ready(function(){
 	var iFrameDOM = $("iframe#comments").contents();
